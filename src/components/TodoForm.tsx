2c01665d3bbba780f3bd7f1cfff96ce8dd1500a0
Dmitriy Panzhinskiy <pandimun@gmail.com>
2021-04-15 15:01:01 +0300

    changed TodoForm / added handler events

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const TodoForm: React.FC = () => {
-  
+  const [ title, setTitle ] = useState<string>('')
+  const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault()
+  };
+  const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+  }
+  const keyPressHandler = (event: React.KeyboardEvent) => {
+    if (event.key === 'Enter') {
+      setTitle("");
+    }
+  }
+
+
   return (
-    <div className="row">
-    <form className="col s12">
+    <div className="row mt2">
+    <form onSubmit={submitHandler} className="col s12">
       <div className="row">
         <div className="input-field">
           <i className="material-icons prefix">mode_edit</i>
-          <textarea id="icon_prefix2" className="materialize-textarea"></textarea>
-          <label htmlFor="icon_prefix2">First Name</label>
+          <input 
+            onKeyPress={keyPressHandler} 
+            value={title} 
+            onChange={changeHandler} 
+            id="icon_prefix2" 
+            type="text"
+          />
+          <label htmlFor="icon_prefix2"> Введите название дела </label>
         </div>
       </div>
     </form>
