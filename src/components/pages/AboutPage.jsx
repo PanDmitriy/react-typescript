4b42a3fa63b03556e7462aba1688ea2fd1598ac9
Dmitriy Panzhinskiy <pandimun@gmail.com>
2021-04-20 08:48:07 +0300

    added react-router-dom and types/ set up Route

--- src/components/pages/AboutPage.jsx
+++ src/components/pages/AboutPage.jsx
@@ -5,6 +5,7 @@ export const AboutPage = () => {
     <>
       <h2> Страница информации. </h2>
       <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate ex dolorem ad, officia, fuga quaerat minima corrupti, voluptates totam incidunt nihil temporibus ducimus sunt assumenda. Inventore facere est officiis corporis. </p>
+      <button className='btn'>  </button>
     </>
   )
 }
